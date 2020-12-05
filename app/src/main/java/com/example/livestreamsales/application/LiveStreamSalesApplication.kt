72225937bfc7bd39c9165701eac10a07181d5d0d
Lavrikov Pavel <me.michael.flora@gmail.com>
2020-12-05 17:35:33 +0300

    Added LiveStreamSalesApplication.kt and page indication library

---
+++ app/src/main/java/com/example/livestreamsales/application/LiveStreamSalesApplication.kt
@@ -0,0 +1,15 @@
+package com.example.livestreamsales.application
+
+import android.app.Application
+
+class LiveStreamSalesApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+    }
+
+}
