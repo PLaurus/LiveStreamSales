a4e157d7387a3686e14b3a3ec72ad4e31b55ae19
Lavrikov Pavel <me.michael.flora@gmail.com>
2020-12-07 20:49:06 +0300

    Implemented main destination ui

--- app/src/main/java/com/example/livestreamsales/application/LiveStreamSalesApplication.kt
+++ app/src/main/java/com/example/livestreamsales/application/LiveStreamSalesApplication.kt
@@ -1,15 +1,22 @@
 package com.example.livestreamsales.application
 
 import android.app.Application
+import android.content.Context
+import com.facebook.drawee.backends.pipeline.Fresco
 
 class LiveStreamSalesApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        initializeFresco(this)
     }
 
     override fun onTerminate() {
         super.onTerminate()
     }
 
+    private fun initializeFresco(context: Context){
+        Fresco.initialize(context)
+    }
 }
