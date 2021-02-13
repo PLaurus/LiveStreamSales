90b624b6a405e597faf3cde72829952f1ea2a48c
Michael <me.michael.flora@gmail.com>
2021-02-13 18:08:13 +0300

    Changed nested navigation graph on viewPager2 at homeDestination

--- app/src/main/java/tv/wfc/livestreamsales/features/home/ui/HomeFragment.kt
+++ app/src/main/java/tv/wfc/livestreamsales/features/home/ui/HomeFragment.kt
@@ -2,23 +2,20 @@ package tv.wfc.livestreamsales.features.home.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import tv.wfc.livestreamsales.R
 import tv.wfc.livestreamsales.databinding.FragmentHomeBinding
 import tv.wfc.livestreamsales.features.authorizeduser.ui.base.AuthorizedUserFragment
 import tv.wfc.livestreamsales.features.home.di.HomeComponent
+import tv.wfc.livestreamsales.features.home.ui.adapters.HomePagesAdapter
 import tv.wfc.livestreamsales.features.home.viewmodel.IHomeViewModel
 import javax.inject.Inject
 
 class HomeFragment: AuthorizedUserFragment(R.layout.fragment_home) {
-    private val homeNavigationController by lazy {
-        requireActivity().findNavController(R.id.homeDestinationNavigationFragment)
-    }
-
-    private var viewBinding: FragmentHomeBinding? = null
-
     lateinit var homeComponent: HomeComponent
         private set
 
@@ -38,12 +35,47 @@ class HomeFragment: AuthorizedUserFragment(R.layout.fragment_home) {
 
     override fun onDataIsPrepared() {
         super.onDataIsPrepared()
+        initializeViewPager()
         initializeBottomNavigation()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         unbindView()
+    }
+
+    private var viewBinding: FragmentHomeBinding? = null
+
+    private val onPageSelectedFromViewPager = object: ViewPager2.OnPageChangeCallback(){
+        override fun onPageSelected(position: Int) {
+            val bottomNavigation = viewBinding?.bottomNavigation ?: return
+
+            val menuItem = when(position){
+                0 -> bottomNavigation.menu.findItem(R.id.mainPage)
+                1 -> bottomNavigation.menu.findItem(R.id.notificationsPage)
+                2 -> bottomNavigation.menu.findItem(R.id.categoriesPage)
+                3 -> bottomNavigation.menu.findItem(R.id.settingsPage)
+                else -> return
+            }
+
+            menuItem.isChecked = true
+        }
+    }
+
+    private val onNavigationItemSelected = object : BottomNavigationView.OnNavigationItemSelectedListener{
+        override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+            val viewPager = viewBinding?.viewPager ?: return false
+
+            viewPager.currentItem = when(menuItem.itemId){
+                R.id.mainPage -> 0
+                R.id.notificationsPage -> 1
+                R.id.categoriesPage -> 2
+                R.id.settingsPage -> 3
+                else -> return false
+            }
+
+            return true
+        }
     }
 
     private fun initializeHomeComponent(){
@@ -62,7 +94,16 @@ class HomeFragment: AuthorizedUserFragment(R.layout.fragment_home) {
         viewBinding = null
     }
 
+    private fun initializeViewPager(){
+        viewBinding?.viewPager?.apply{
+            isUserInputEnabled = false
+            (getChildAt(0) as RecyclerView).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+            adapter = HomePagesAdapter(this@HomeFragment)
+            registerOnPageChangeCallback(onPageSelectedFromViewPager)
+        }
+    }
+
     private fun initializeBottomNavigation(){
-        viewBinding?.bottomNavigation?.setupWithNavController(homeNavigationController)
+        viewBinding?.bottomNavigation?.setOnNavigationItemSelectedListener(onNavigationItemSelected)
     }
 }
---
+++ app/src/main/java/tv/wfc/livestreamsales/features/home/ui/adapters/HomePagesAdapter.kt
@@ -0,0 +1,21 @@
+package tv.wfc.livestreamsales.features.home.ui.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import tv.wfc.livestreamsales.features.categories.ui.CategoriesFragment
+import tv.wfc.livestreamsales.features.mainpage.ui.MainPageFragment
+import tv.wfc.livestreamsales.features.notifications.ui.NotificationsFragment
+import tv.wfc.livestreamsales.features.settings.ui.SettingsFragment
+
+class HomePagesAdapter(fragment: Fragment): FragmentStateAdapter(fragment){
+    override fun getItemCount(): Int = 4
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0 -> MainPageFragment()
+            1 -> NotificationsFragment()
+            2 -> CategoriesFragment()
+            else -> SettingsFragment()
+        }
+    }
+}
